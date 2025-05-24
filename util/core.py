09e845355b95101eae5b694126f5f4d2387aca23
dimalei <dima.leigr@gmail.com>
2025-05-24 12:34:43 +0200

    nice vector print

--- util/core.py
+++ util/core.py
@@ -7,6 +7,7 @@ import scipy.linalg as sp
 from random import random, randint
 from typing import Iterable, Callable
 import math
+from IPython.display import display, Math
 
 DECIMAL_PRECISION = 10
 
@@ -47,16 +48,28 @@ def SortRows(Aa):
     return Aa[inx.argsort()]
 
 
-def mrref(Aa_in, verbos=0):
+def mrref(Aa_in: np.ndarray, verbos=0):
+    """
+    Computes a modified row-reduced echelon form (RREF) of a matrix.
+
+    Parameters:
+        Aa_in (np.ndarray): The input matrix to be row reduced.
+        verbos (int): Verbosity level for debug printing (default: 0).
+
+    Returns:
+        np.ndarray: Matrix in modified RREF form.
+    """
     Aa = Aa_in * 1.0
     Nn = len(Aa)
     kklist = np.arange(0, Nn - 1)
-    # print('kklist', kklist)
+
+    # print("kklist", kklist)
+
     for kk in kklist:
         Aa[kk:, kk:] = eliminate(Aa[kk:, kk:], verbos=verbos - 1)
     Aa = SortRows(Aa)
     Aa = np.flipud(Aa)
-    # for kk in kklist:
+
     for kkh in kklist:
         kk = FirstNonZero(Aa[kkh, :])
         Aa[kkh::, kk::] = eliminate(Aa[kkh::, kk::], fix=True, verbos=verbos - 1)
@@ -64,6 +77,20 @@ def mrref(Aa_in, verbos=0):
 
 
 def mnull(Aa_in, leps=np.finfo(float).eps * 10, verbos=0):
+    """
+    Nimmt die Koeffizientenmatrix von homogenen Gleichungsysteme und gibt die
+    Richtungsvektoren zurück (bzw. Richtungsvektoren des Nullraums)
+
+    Parameters:
+        Aa_in (np.ndarray): Input matrix whose null space is to be computed.
+        leps (float): Threshold below which values are considered zero (default: 10×machine epsilon).
+        verbos (int): Verbosity level for debug printing (default: 0).
+
+    Returns:
+        np.ndarray: Richtungsvektor aus der Homogenen Gleichung.
+                    A matrix whose columns form a basis for the null space of `Aa_in`.
+
+    """
     Aa = mrref(Aa_in)
     Aa = Aa[list(map(np.linalg.norm, Aa)) > leps]  # extract non-zero linies
     mpiv = np.array(Aa[0] * 0, dtype=bool)
@@ -93,24 +120,37 @@ def mnull(Aa_in, leps=np.finfo(float).eps * 10, verbos=0):
 
 
 def linear_combination(*vectors: list):
-    try:
-        lgs = np.array(vectors)
-    except ValueError:
-        print("Vektoren haben nicht die gleiche Dimension!")
-        return
+    """
+    Checks if input vectors are linearly depended (co-planar)
+    and prints the linear combination to create the null-vector
 
-    if lgs.size <= 1:
+    Parameters:
+        *vectors (list): Input vectors. Must be of the same dimesnion.
+
+    """
+    # assemble matrix from vectors
+    if not isinstance(vectors, np.ndarray):
+        try:
+            matrix = np.array(vectors)
+        except ValueError:
+            print("Vektoren haben nicht die gleiche Dimension!")
+            return
+    # unless it's already a matrix
+    else:
+        matrix = vectors
+
+    if matrix.size <= 1:
         print("Mehr als 1 Vektor nötig!")
         return
 
     # generate LGS
     # print(lgs)
-    rows, cols = lgs.shape
-    lgs = np.concatenate([lgs, np.eye(rows, cols)], axis=1)
+    rows, cols = matrix.shape
+    matrix = np.concatenate([matrix, np.eye(rows, cols)], axis=1)
     # print(lgs)
 
     # eliminate components
-    result = mrref(lgs)
+    result = mrref(matrix)
 
     # extract last vector
     last_row = result[-1]
@@ -131,7 +171,11 @@ def linear_combination(*vectors: list):
 def check_linearity(input_dimensions: int, transformation: Callable):
     """
     Checks if a transformation is linear or not by applying random real values.
-    Usage: check_linearity(2, lambda v: [math.cos(random()) * v[0], math.sin(random()) * v[1]])
+    Prints results directly into console.
+
+    Parameters:
+        input_dimensions (int): expected input dimensions of the transformation
+        transformation (Callable): lambda expression representing the transformation
     """
     DECIMAL_PRECISION = 8
 
@@ -188,7 +232,11 @@ def is_null(vector) -> bool:
 
     # check vector null
     for number in vector:
-        if number != 0:
+        try:
+            if number != 0:
+                return False
+        except:
+            # return false input is a matrix
             return False
     return True
 
@@ -199,6 +247,76 @@ def random_vector(dimension: int, R=True) -> list:
     return [randint(-99999, 99999) for i in range(dimension)]
 
 
+def latex_vector(dimension: int, components=[]):
+    """
+    returns a String that can be rendered by katex as a vector.
+    Parameters:
+        dimension (int): number of components of the latex_vector
+        components (Number): value of that component, null if variable (renderd as x_1, x_2)
+
+    Returns:
+        String
+    """
+    # cast to list if components is np.narray
+    if isinstance(components, np.ndarray):
+        components = components.tolist()
+
+    # fill up None components if missing
+    components += [None] * (dimension - len(components))
+    # write latex string
+    out = "\\begin{pmatrix}"
+    for i in range(dimension):
+        out += (
+            f"x_{i+1}\\\\"
+            if components[i] == None
+            else f"{components[i]:.3f}".rstrip("0").rstrip(
+                "."
+            )  # don't print unnecassary decimals
+            + "\\\\"
+        )
+    out += "\\end{pmatrix}"
+    return out
+
+
+def latex_directional_vector(dimension: int, components, index=0):
+    """
+    returns a String that renders latex vectors with variable scalars
+    Parameters:
+        index (int): defines the name of the scalar
+    """
+    scalars = [
+        r"\lambda",
+        r"\mu",
+        r"\gamma",
+    ]
+    index = index % len(scalars)
+    return scalars[index] + r"\cdot" + latex_vector(dimension, components)
+
+
+def null_space(lgs: np.ndarray, verbos=0):
+
+    dimensionen = lgs.shape[1]
+    richtungsvektoren = mnull(lgs)
+    if verbos > 0:
+        print(richtungsvektoren)
+
+    if is_null(richtungsvektoren):
+        print(f"Triviale Lösung: {richtungsvektoren}")
+        return
+
+    anzahl_richtungsvektoren = richtungsvektoren.shape[1]
+
+    out = latex_vector(dimensionen) + "="
+    for i in range(anzahl_richtungsvektoren):
+
+        out += ("+" if i != 0 else "") + latex_directional_vector(
+            dimensionen, richtungsvektoren[:, i], i
+        )
+
+    display(Math(out))
+
+
 # testing
 if __name__ == "__main__":
+
     pass
